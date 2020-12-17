d8d47632639fe69daf836f59d75dc7f8fc0abc51
Gaspard <gaspard.debussche@ensimag.fr>
2020-12-17 00:00:59 -0500

    Solution

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -31,11 +31,50 @@ describe('ListComponent', () => {
     it('should have a phone', () => {
       expect(component.unselectedProviders[0].phone).toEqual('8991234321');
     });
+
+    it('selectProviders() called should decrease unselected by one', () => {
+      const size_unselected = component.unselectedProviders.length;
+      component.clickProvider('1');
+      component.selectProviders();
+      expect(component.unselectedProviders.length).toEqual(size_unselected - 1);
+    });
+
+    it('selectProviders() called should decrease unselected by two', () => {
+      const size_unselected = component.unselectedProviders.length;
+      component.clickProvider('1');
+      component.clickProvider('2');
+      component.selectProviders();
+      expect(component.unselectedProviders.length).toEqual(size_unselected - 2);
+    });
+
+    it('selectProviders() called should increase selected by one', () => {
+      const size_selected = component.selectedProviders.length;
+      component.clickProvider('1');
+      component.selectProviders();
+      expect(component.selectedProviders.length).toEqual(size_selected + 1);
+    });
+
+    it('selectProviders() called should increase selected by two', () => {
+      const size_selected = component.selectedProviders.length;
+      component.clickProvider('1');
+      component.clickProvider('2');
+      component.selectProviders();
+      expect(component.selectedProviders.length).toEqual(size_selected + 2);
+    });
   });
 
   describe('selected providers', () => {
     it('should have no initial length', () => {
       expect(component.selectedProviders.length).toEqual(0);
+    });
+
+    it('unselectProvider() called', () => {
+      let size_selected = component.selectedProviders.length;
+      component.clickProvider('1');
+      component.selectProviders();
+      size_selected++;
+      component.unselectProvider(0);
+      expect(component.selectedProviders.length).toEqual(size_selected - 1);
     });
   });
 });
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-list',
@@ -29,8 +29,73 @@ export class ListComponent implements OnInit {
     }
   ];
 
+  public clickedProviders = [];
+
+
+  clickProvider = (providerId) => {
+    const foundIndex = this.clickedProviders.findIndex(( index ) => index === this.unselectedProviders[providerId].id)
+    if (foundIndex > -1) {
+      this.clickedProviders.splice(foundIndex, 1)
+    } else {
+      this.clickedProviders.push(this.unselectedProviders[providerId].id)
+    }
+  }
+
+
+  selectProviders = () => {
+    this.clickedProviders.forEach((providerId) => {
+      const foundIndex = this.unselectedProviders.findIndex(({ id }) => id === providerId)
+      this.selectedProviders.push(this.unselectedProviders[foundIndex]);
+      this.unselectedProviders.splice(foundIndex, 1);
+    })
+
+    this.clickedProviders = [];
+
+    sessionStorage.setItem('unselectedProviders', JSON.stringify(this.unselectedProviders));
+    sessionStorage.setItem('selectedProviders', JSON.stringify(this.selectedProviders));
+  }
+
+  unselectProvider = (providerId) => {
+    const unselectedProvider = this.selectedProviders[providerId];
+    function insertSorted(insertedProvider, arr) {
+      let isLast = true;
+      if(arr.length === 0)
+        arr.push(insertedProvider)
+      else {
+        for (let i = 0, len = arr.length; i < len; i++) {
+          if (parseInt(insertedProvider.id) < parseInt(arr[i].id)) {
+            isLast = false;
+            arr.splice(i, 0, insertedProvider);
+            break;
+          }
+        }
+        if(isLast){
+          arr.push(insertedProvider);
+        }
+      }
+      return arr;
+    }
+
+    this.unselectedProviders = insertSorted(unselectedProvider, this.unselectedProviders);
+    this.selectedProviders.splice(providerId, 1);
+
+    sessionStorage.setItem('unselectedProviders', JSON.stringify(this.unselectedProviders));
+    sessionStorage.setItem('selectedProviders', JSON.stringify(this.selectedProviders));
+  }
+
+  highlightClickedProvider = (providerId) => {
+    if (this.clickedProviders.includes(this.unselectedProviders[providerId].id)) {
+      return "table-primary";
+    }
+  }
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (sessionStorage.getItem('unselectedProviders') !== null) {
+      this.unselectedProviders = JSON.parse(sessionStorage.getItem('unselectedProviders'));
+      this.selectedProviders = JSON.parse(sessionStorage.getItem('selectedProviders'));
+    }
+  }
 
 }
